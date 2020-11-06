3c2fa33434f24784a9c7b562125b4cc8f577a11a
Sergio Rodríguez <sergio.rodriguezvazquez@alum.uca.es>
2020-11-06 18:35:13 +0100

    Practica terminada :)

--- Practicas/practicas-da-64-1.6/Rodriguez_Vazquez_Sergio_DA/p1/DefenseStrategy.cpp
+++ Practicas/practicas-da-64-1.6/Rodriguez_Vazquez_Sergio_DA/p1/DefenseStrategy.cpp
@@ -28,7 +28,6 @@ struct C
     float value;
 };
 
-
 Vector3 cellCenterToPosition(int i, int j, float cellWidth, float cellHeight)
 {
     return Vector3((j * cellWidth) + cellWidth * 0.5f, (i * cellHeight) + cellHeight * 0.5f, 0);
@@ -40,11 +39,39 @@ void positionToCell(const Vector3 pos, int &i_out, int &j_out, float cellWidth, 
     j_out = (int)(pos.x * 1.0f / cellWidth);
 }
 
-float cellValue(int row, int col, bool **freeCells, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight, List<Object *> obstacles, List<Defense *> defenses)
-{ 
-    
-    return 0; // implemente aqui la funci�n que asigna valores a las celdas
+
+float cellValue(int row, int col, int nCellsWidth, int nCellsHeight, float cellWidth, float cellHeight)
+{     
+    // Vector3 center = cellCenterToPosition(nCellsWidth/2, nCellsHeight/2, cellWidth, cellHeight);
+
+    // return _distance(cellCenterToPosition(row, col, cellWidth, cellHeight), center);
+    Vector3
+    cornerUpLeft = cellCenterToPosition(0, 0, cellWidth, cellHeight),
+    cornerUpRight = cellCenterToPosition(0, nCellsWidth, cellWidth, cellHeight),
+    cornerDownLeft = cellCenterToPosition(nCellsHeight, 0, cellWidth, cellHeight),
+    cornerDownRight = cellCenterToPosition(nCellsHeight, nCellsWidth, cellWidth, cellHeight);
+
+
+    // float CUpLeftDistance, CUpRightDistance, CDownLeftDistance, CDownRightDistance;
+    float
+    CUpLeftDistance = _distance(cellCenterToPosition(row, col, cellWidth, cellHeight), cornerUpLeft),
+    CUpRightDistance = _distance(cellCenterToPosition(row, col, cellWidth, cellHeight), cornerUpRight),
+    CDownLeftDistance = _distance(cellCenterToPosition(row, col, cellWidth, cellHeight), cornerDownLeft),
+    CDownRightDistance = _distance(cellCenterToPosition(row, col, cellWidth, cellHeight), cornerDownRight);
+
+    return std::min({CUpLeftDistance, CUpRightDistance, CDownLeftDistance, CDownRightDistance});
+
+    // return std::max(std::max(CUpLeftDistance, CUpRightDistance), std::max(CDownLeftDistance, CDownRightDistance)); //Pondriamos esto para calcular la distancia a una de las esquinas
+    //Lo que falta en esta funcion es ordenar las distancias a las esquinas y devolver la mayor, en base a eso hay que hacer otra funcion cellValue que ponga puntuacion a las demas defensas
+
+     // implemente aqui la funci�n que asigna valores a las celdas
 }
+
+float cellValue(int row, int col, float cellWidth, float cellHeight, Vector3 position)
+{
+    return _distance(cellCenterToPosition(row, col, cellWidth, cellHeight), position); 
+}
+
 
 bool factible(std::list<Defense *> defenses, std::list<Object *> obstacles, float mapWidth, float mapHeight, Vector3 position, float radio, bool **freeCells, int CellsWidth, int CellsHeight, int ultima_colocada)
 {
@@ -87,7 +114,6 @@ void DEF_LIB_EXPORTED placeDefenses(bool **freeCells, int nCellsWidth, int nCell
     float x, y;
     int ultima_colocada = -1;
 
-
     List<C> Candidates;
 
     //Hacemos lista de celdas (candidatos) para colocar el resto de defensas    
@@ -95,7 +121,7 @@ void DEF_LIB_EXPORTED placeDefenses(bool **freeCells, int nCellsWidth, int nCell
     {
         for(int j = 0; j < nCellsWidth; j++)
         {
-            C c({cellCenterToPosition(i, j, cellWidth, cellHeight)}, cellValue(i, j, freeCells, nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles, defenses));
+            C c({cellCenterToPosition(i, j, cellWidth, cellHeight)}, cellValue(i, j, nCellsWidth, nCellsHeight, cellWidth, cellHeight));
             Candidates.push_back(c);
         }
     }
@@ -106,15 +132,48 @@ void DEF_LIB_EXPORTED placeDefenses(bool **freeCells, int nCellsWidth, int nCell
     // }
 
     Candidates.sort(isMinor);
+    C cfirstpromising;
+
+
+    List<Defense*>::iterator currentDefense = defenses.begin();
+    while(!Candidates.empty() && ultima_colocada == -1 )
+    { 
+        cfirstpromising = Candidates.back();
+        Candidates.pop_back();
+        if(factible(defenses, obstacles, mapWidth, mapHeight, cfirstpromising.position, (*currentDefense)->radio, freeCells, cellWidth, cellHeight, -1))
+        {
+            (*currentDefense)->position.x = cfirstpromising.position.x;
+            (*currentDefense)->position.y = cfirstpromising.position.y;
+            (*currentDefense)->position.z = 0;
+            ultima_colocada++;
+
+        }
+    }
+    Candidates = {};
+
+    //Hemos colocado la primera defensa
     
-    //Ponemos el resto de Defensas
-    List<Defense *>::iterator currentDefense = defenses.begin();
+    // List<C> Candidates;
+
+
+
+    for(int i = 0; i < nCellsHeight; i++)
+    {
+        for(int j = 0; j < nCellsWidth; j++)
+        {
+            C c({cellCenterToPosition(i, j, cellWidth, cellHeight)}, cellValue(i, j, cellWidth, cellHeight, (*currentDefense)->position));
+            Candidates.push_back(c);
+        }
+    }
+
+    ++currentDefense;
+    Candidates.sort(isMinor);
+    //Ponemos las demas defensas
     C cpromising;
     while(currentDefense != defenses.end() && !Candidates.empty())
     {
         cpromising = Candidates.front();
         Candidates.pop_front();
-
         if(factible(defenses, obstacles, mapWidth, mapHeight, cpromising.position, (*currentDefense)->radio, freeCells, cellWidth, cellHeight, ultima_colocada))
         {
             (*currentDefense)->position.x = cpromising.position.x;
